a381e0ced5b2062f5a9b3e742ca53d7828b14b4e
baoyipin <baoyipin@126.com>
2018-10-21 10:35:42 +0800

    cloudmusic

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import React,{Component} from "react";
+import {HashRouter as Router,Link,Route} from "react-router-dom";
+import Home from "./container/home/home";
+import Login from "./container/login/login";
+export default class App extends Component{
+    render(){
+        return(
+           <Router>
+               <div>
+                   <Route path="/" exact component={Home}></Route>
+                   <Route path="/login" component={Login}></Route>
+               </div>
+           </Router>
+        )
+    }
 }
-
-export default App;
